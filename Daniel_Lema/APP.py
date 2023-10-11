af726919d42a4820fe29d2d8911597c9f0cb104c
Dande8719 <103025222+Dande8719@users.noreply.github.com>
2023-10-11 11:13:01 +0200

    v1

---
+++ Daniel_Lema/APP.py
@@ -0,0 +1,46 @@
+import streamlit as st
+from streamlit_lottie import st_lottie
+import requests
+
+def app():
+    st.title('CURRENCY CONVERTER') 
+
+
+    # st.markdown('<h1 style="font-size: 40px; text-align: justify;">CURRENCY CONVERTER</h1>', unsafe_allow_html=True)
+    st.markdown('<h1 style="font-size: 20px; text-align: justify;">Currency converter and money value predictor</h1>', unsafe_allow_html=True)
+    
+    st_lottie(requests.get("https://lottie.host/5d6e1b3c-deb7-4145-ac9c-0513f0dab97f/332WhxMQk1.json").json(), height=250, key="Into1")
+    
+
+    Ciudad=st.sidebar.selectbox("Elige Moneda a cambiar", 
+                                ["EURO",
+                                 "DOLLAR"])
+    
+    Ciudad=st.sidebar.selectbox("Elige Moneda a recibir", #tenemos que hacer codigo para no poder meter la moneda escrita enteriormente
+                                ["EURO",
+                                 "DOLLAR"])
+    
+    if Ciudad == "EURO":
+
+        st.write('<div style="text-align: justify;"> Escoge las características de tu vivienda en Madrid para obtener un precio .</div>', unsafe_allow_html=True)  
+        # Slider de selección m2.
+        st.slider("Selecciona los metros cuadrados de tu vivienda:", min_value=0, max_value=300, value=75, step=1)    
+        
+        
+
+        
+
+    elif Ciudad == "DOLLAR":
+
+
+# Slider de selección Clasificación.
+        Clasificacion= ['En trámite','No indicado','Disponible','Pendiente de completar','Exento']
+        st.selectbox("Selecciona si tiene el certificado energético:", Clasificacion)       
+
+
+
+
+
+
+if __name__ == "__APP__":
+    app()    
